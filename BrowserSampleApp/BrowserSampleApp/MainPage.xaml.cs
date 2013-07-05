1395fcb1c7562e9ac8edaf5687e746a2543bdba9
sgrebnov <sergei.grebnov@gmail.com>
2013-07-05 09:53:29 +0400

    fixes #2 "GoBack functionality"

--- BrowserSampleApp/BrowserSampleApp/MainPage.xaml.cs
+++ BrowserSampleApp/BrowserSampleApp/MainPage.xaml.cs
@@ -19,7 +19,7 @@ namespace BrowserSampleApp
             InitializeComponent();
 
             // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+            BuildLocalizedApplicationBar();
         }
 
         private void OpenSite(string siteUri)
@@ -34,20 +34,36 @@ namespace BrowserSampleApp
         }
 
         // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        private void BuildLocalizedApplicationBar()
+        {
+            // Set the page's ApplicationBar to a new instance of ApplicationBar.
+            ApplicationBar = new ApplicationBar();
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+            // Create a new button and set the text value to the localized string from AppResources.
+            ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+            appBarButton.Text = AppResources.AppBarBackButtonText;
+            ApplicationBar.Buttons.Add(appBarButton);
 
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+            appBarButton.Click += appBarButton_Click;
+
+            //// Create a new menu item with the localized string from AppResources.
+            //ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+            //ApplicationBar.MenuItems.Add(appBarMenuItem);
+        }
+
+        /// <summary>
+        /// TODO comments
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void appBarButton_Click(object sender, EventArgs e)
+        {
+            if (WebBrowserWindow.CanGoBack)
+            {
+
+                WebBrowserWindow.GoBack();
+            }
+        }
 
         /// <summary>
         /// Implements functionality to open site uri in browser control.
